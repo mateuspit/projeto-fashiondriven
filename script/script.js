f994d2f11fcd2bf1148034af14198422df6fe194
Barcelos <mateuspit@hotmail.com>
2023-02-01 20:53:07 -0300

    Validation of user inputs, done; Next: send user data to API

--- script/script.js
+++ script/script.js
@@ -1,7 +1,10 @@
 let optionModelSelected = false;
 let optionCollarSelected = false;
 let optionFabricSelected = false;
+let inputLinkWritted = false;
 let openFinishButton = false;
+
+
 
 
 function selectingOptionModel(elementSelected){
@@ -35,4 +38,32 @@ function selectingOptionFabric(elementSelected){
     let elementFabric = elementSelected.querySelector('[class*="Img"]');
     elementFabric.classList.add("selected"); 
     optionFabricSelected = true;
+}
+
+function linkValidation(){
+    let valueInputReferenceLink = document.getElementById("referenceInputLink").value;
+    let endLinkReversed = "";
+    for(let i = (valueInputReferenceLink.length - 1); i>(valueInputReferenceLink.length - 6); i--){
+        endLinkReversed += valueInputReferenceLink[i];
+    }    
+    let endLink =  endLinkReversed.split('').reverse().join('');
+    inputLinkWritted = endLink.includes(".jpeg") || endLink.includes(".png") || endLink.includes(".jpg");
+}
+
+function finishButtonValidation(){
+    openFinishButton = optionModelSelected && optionCollarSelected && optionFabricSelected && inputLinkWritted;
+}
+
+
+function finishOrder(){
+    linkValidation();
+    finishButtonValidation();
+    // console.log(openFinishButton);
+    if(openFinishButton){
+        alert("Seu pedido foi enviado!");
+    }
+    else {
+        alert("Por favor, escolha o modelo, a gola, o tecido e insira um link valido");
+    }
+
 }
